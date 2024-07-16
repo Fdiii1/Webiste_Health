a2088d261b2fcdb7968c7f5cfa64d67eabe012ba
Fdiii1 <phat76997@gmail.com>
2024-07-16 23:35:10 +0700

    namtu

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -15,10 +15,10 @@ class About extends Component {
                 <div className='content-left'>
                 <iframe width="100%" height="400px" src="https://www.youtube.com/embed/ezh7rQaDoJ4?list=PLofk0Qws87XWFsnDn_-37Ay4-lKM0rhwa" 
                 title="2 Ngày 1 Đêm Việt Nam | Tập 3: Dương Lâm &quot;ngã quỵ&quot; trên sân với cú &quot;vô lê&quot; đẳng cấp từ Cris Phan"
-                frameborder="0" 
+                frameBorder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerpolicy="strict-origin-when-cross-origin" 
-                allowfullscreen></iframe>
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen></iframe>
                 </div>
             <div className='content-right'>
                 <p>
