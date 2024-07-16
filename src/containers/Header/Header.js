b80e7b2178f055741a37371a48016af10ca564b5
Fdiii1 <phat76997@gmail.com>
2024-07-16 23:15:49 +0700

    namba

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -5,11 +5,15 @@ import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
+import {LANGUAGE} from'../../utils';
 
 class Header extends Component {
 
+    handleChangeLanguage = (language) =>{
+        this.props.changLanguageAppRedux(language)
+    }
     render() {
-        const { processLogout } = this.props;
+        const { processLogout , language } = this.props;
 
         return (
             <div className="header-container">
@@ -17,9 +21,17 @@ class Header extends Component {
                 <div className="header-tabs-container">
                     <Navigator menus={adminMenu} />
                 </div>
-
+                <div className='languages'>
+                        <span 
+                        className={language === LANGUAGE.VI ? 'language-vi active': 'language-vi'}
+                        onClick={()=>this.handleChangeLanguage(LANGUAGE.VI)}>VN
+                            </span>
+                        <span className={language === LANGUAGE.EN ? 'language-en active': 'language-en'}
+                        onClick={()=>this.handleChangeLanguage(LANGUAGE.EN)}>EN
+                        </span>
+                </div>
                 {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
+                <div className="btn btn-logout" onClick={processLogout} title='Log out'>
                     <i className="fas fa-sign-out-alt"></i>
                 </div>
             </div>
@@ -30,13 +42,15 @@ class Header extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         processLogout: () => dispatch(actions.processLogout()),
+        changLanguageAppRedux: (language) => dispatch(actions.changLanguageApp(language))
     };
 };
 
